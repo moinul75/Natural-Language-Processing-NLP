9c3136c24dbf53d3b316f8b629bbbdd7df6f4a7b
moinul75 <freelancerudoy752@gmail.com>
2023-10-23 21:51:43 +0600

    update the re

--- RegularExpression/sting_match.py
+++ RegularExpression/sting_match.py
@@ -4,15 +4,20 @@ Pattern Matching :
 Pattern    Exmaple          Description 
 [A-Z]      UDOY             Match Only the Capital Latter 
 [0-9]      123456           Match Only Numeric values 
-[A-Za-z]   Udoy             Match Only Capital and Smaller value 
+[A-Za-z]   Udoy             Match Only Capital and Smaller value not the numeric vlues 
 [Hmm**]    Hmm,Hmmm,Hmmm..  0 or more matching 
+Kleene and Clousure 
+'+' : 1 or more 
+'*' : 0 or more 
+Example: new(s)* result is : new, news, newss, newssssss, news....
+Example: news(s)+: result is : news, newsss, newss... 
 """
 import re 
 
 #String Match
 pattern = 'Udoy'
 
-match = re.search(pattern,"My Name is aBC")
+match =re.match(pattern,"My Name is aBC")
 
 if match: 
     print("Result is Matched")
@@ -41,6 +46,56 @@ if matching:
 
 else: 
     print('Result is not matching')
+
+
+
+#now kleene and Clousure 
+
+wrd = 'new(s)*'
+result = re.match(wrd,'news')
+
+if result: 
+    print(f'Pattern Matched {result}')
+else: 
+    print('Not matched')
+
+nwrd = 'new(s)+'
+
+result = re.match(nwrd,"new")
+
+if result: 
+    print(f'Pattern Matched {result}')
+else: 
+    print('Not matched')
+
+
+# * and + in the middle (lo(v)+e)
+
+ptrn = 'lo(v)+e'
+
+result = re.match(ptrn,'lovve')
+
+if result: 
+    print(f'Pattern Matched {result}')
+
+else: 
+    print(f'not Matched')
+
+
+#pipe sysmbol (|) work like Or logical Operator N.B: if a|b , and the pattern is ab , it not matched if and only there is either a or b (a/b) not ab 
+ptrn = 's(u|i|n)m'
+
+result = re.match(ptrn,"sum")
+
+if result: 
+    print(f'Pattern matched {result}')
+
+else: 
+    print('Pattern Not matched')
+
+
+
+
 
 
 
